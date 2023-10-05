e89f7d0e35ff0da74d7c856d02e86ca0a1c5cf2c
MansimarUSC <136744767+MansimarUSC@users.noreply.github.com>
2023-10-04 17:32:29 -0700

    Added player jump
    Commit details:
    
    1. Added player jump
    2. Added the to our main scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     float moveSpeed = 3.0f;
+    float jumpForce = 5.0f;
+
+    Rigidbody2D rb;
+    bool isGrounded = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f;
     }
 
     // Update is called once per frame
@@ -32,8 +36,34 @@ public class Player : MonoBehaviour
             transform.Translate(0, -moveSpeed * Time.deltaTime, 0);
         }
 
+        CheckGrounded();
 
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
     }
+
+    void CheckGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
+
+        if (hit.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        rb.gravityScale = 1f;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag=="Maze")
