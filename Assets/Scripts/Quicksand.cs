6f5fe1c661dd92c74d22bb35e81ed485601cd751
ojasvinaik13 <ojasvi.naik@gmail.com>
2023-10-15 18:27:37 -0700

    Quicksand added in SampleSceneCopy_Ojasvi

--- Assets/Scripts/Quicksand.cs
+++ Assets/Scripts/Quicksand.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class Quicksand : MonoBehaviour
 {
     private bool isSinking = false;
-    private float sinkSpeed = 0.05f;
-    private float sinkDepth = 2.0f;
+    private float sinkSpeed = 0.5f;
+    //private float sinkDepth = 2.0f;
     private Transform playerTransform;
+    private float startingYPosition;
+    private float groundYPosition;
+    float sinkingProgress = 1f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player1"))
+        if (collision.CompareTag("Player"))
         {
             Debug.Log("COLLISION");
             isSinking = true;
             Rigidbody2D playerRigidbody = collision.GetComponent<Rigidbody2D>();
             if (playerRigidbody != null)
             {
-                Debug.Log("PLAYER1");
-                playerRigidbody.gravityScale = 0.001f; 
-                playerRigidbody.drag = 10f;
+                //Debug.Log("PLAYER1");
+                //playerRigidbody.gravityScale = 0.001f; 
+                //playerRigidbody.drag = 20f;
+                sinkingProgress = 0f;
             }
         }
     }
@@ -40,23 +44,45 @@ public class Quicksand : MonoBehaviour
     }*/
     void Start()
     {
-        GameObject player = GameObject.FindWithTag("Player1");
+        GameObject player = GameObject.FindWithTag("Player");
         if (player != null)
         {
             playerTransform = player.transform;
+            startingYPosition = playerTransform.position.y;
         }
     }
 
     void Update()
     {
-        GameObject player = GameObject.FindWithTag("Player1");
+        GameObject player = GameObject.FindWithTag("Player");
         playerTransform = player.transform;
-        if (isSinking && playerTransform != null)
+        startingYPosition = playerTransform.position.y;
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit))
+        {
+            groundYPosition = hit.point.y;
+        }
+        /*if (isSinking && playerTransform != null)
         {
             Vector3 playerPosition = playerTransform.position;
             // playerPosition.y -= sinkSpeed * Time.deltaTime;
             //playerPosition.y = Mathf.Max(playerPosition.y, initialPosition.y - sinkDepth);
             playerTransform.position = playerPosition;
+        }*/
+        if (sinkingProgress < 1.0f)
+        {
+            sinkingProgress += Time.deltaTime * sinkSpeed; // Adjust sinkingSpeed as needed.
+            Vector3 newPosition = playerTransform.position;
+            newPosition.y = Mathf.Lerp(startingYPosition, groundYPosition, sinkingProgress);
+            playerTransform.position = newPosition;
+            float scale = Mathf.Lerp(1f, 0f, sinkingProgress); // Adjust the scale values as needed.
+            playerTransform.localScale = new Vector3(1f, scale, 1f);
+        }
+        if (sinkingProgress > 1.0f)
+        {
+            // Once the player has fully sunk, destroy the player object.
+            Destroy(player);
+            Debug.Log("Player SUNK");
         }
     }
 }
