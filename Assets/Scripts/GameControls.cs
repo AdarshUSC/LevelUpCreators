2d5fb76e1f7b1b87e3aab09cd5f56a144b28b323
MansimarUSC <136744767+MansimarUSC@users.noreply.github.com>
2023-10-17 21:01:00 -0700

    Game control text
    1. Game control initial text
    2. Camera setting
    3. Other UI cleanup

---
+++ Assets/Scripts/GameControls.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameControls : MonoBehaviour
+{
+    [SerializeField] public TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = "Press Tab - Reflection, 'B' - Boomerang<br>Hint!! -- Change the color to save yourself from the bullets.";
+    }
+
+}
