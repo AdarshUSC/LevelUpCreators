911194f10f1c4e3ada65d4d699552e892b73b828
ojasvinaik13 <ojasvi.naik@gmail.com>
2023-10-19 21:59:44 -0700

    Quicksand placement minor changes

--- Assets/Scripts/Quicksand.cs
+++ Assets/Scripts/Quicksand.cs
@@ -8,9 +8,13 @@ public class Quicksand : MonoBehaviour
     private float sinkSpeed = 0.5f;
     //private float sinkDepth = 2.0f;
     private Transform playerTransform;
+    private Transform scale1;
     private float startingYPosition;
+    private Player p;
     private float groundYPosition;
     float sinkingProgress = 1f;
+    private Vector3 originalPlayerScale;
+    private Vector3 originalGroundPosition;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -45,10 +49,18 @@ public class Quicksand : MonoBehaviour
     void Start()
     {
         GameObject player = GameObject.FindWithTag("Player");
+        p = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        
         if (player != null)
         {
             playerTransform = player.transform;
             startingYPosition = playerTransform.position.y;
+            originalPlayerScale = playerTransform.localScale;
+        }
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit))
+        {
+            originalGroundPosition = hit.point;
         }
     }
 
@@ -77,11 +89,18 @@ public class Quicksand : MonoBehaviour
             playerTransform.position = newPosition;
             float scale = Mathf.Lerp(1f, 0f, sinkingProgress); // Adjust the scale values as needed.
             playerTransform.localScale = new Vector3(1f, scale, 1f);
+
+            Vector3 newGroundPosition = transform.position;
+            newGroundPosition.y = Mathf.Lerp(originalGroundPosition.y, originalGroundPosition.y, sinkingProgress); // You want to keep the ground position the same
+            transform.position = newGroundPosition;
         }
         if (sinkingProgress > 1.0f)
         {
-            // Once the player has fully sunk, destroy the player object.
-            Destroy(player);
+            //Destroy(player);
+            sinkingProgress = 1f;
+            gameObject.transform.position = p.respawnPoint;
+            playerTransform.localScale = originalPlayerScale;
+            transform.position = originalGroundPosition;
             Debug.Log("Player SUNK");
         }
     }
