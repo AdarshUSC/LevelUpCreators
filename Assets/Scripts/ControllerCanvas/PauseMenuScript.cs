a7bb50b6a334ad5c9e89bded94fe986ddd88c964
Wuhen Duan <63495442+cl-vv-h@users.noreply.github.com>
2023-10-24 23:33:24 -0700

    PauseMenu Prefab

---
+++ Assets/Scripts/ControllerCanvas/PauseMenuScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    [SerializeField] GameObject PauseMenu;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        PauseMenu.SetActive(false);
+    }
+
+    public void Pause()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
