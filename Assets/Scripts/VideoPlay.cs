19830abbaccf0f8dd9e63234c9fc62f0bff77395
Wuhen Duan <63495442+cl-vv-h@users.noreply.github.com>
2023-10-27 12:23:53 -0700

    use url to play the video

---
+++ Assets/Scripts/VideoPlay.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlay : MonoBehaviour
+{
+    [SerializeReference] VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "tutorial4reflection.mov");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
