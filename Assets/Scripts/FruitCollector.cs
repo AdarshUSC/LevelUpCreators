afdafd68693613491e458733d640d6242ca27d81
luoyuanzzz <luoyuanz@usc.edu>
2023-10-26 18:33:00 -0700

    update tutorial 3

--- Assets/Scripts/FruitCollector.cs
+++ Assets/Scripts/FruitCollector.cs
@@ -9,6 +9,16 @@ public class FruitCollector : MonoBehaviour
     private int collectibles = 0;
     [SerializeField] private TMP_Text CollectiblesText;
 
+    [SerializeField] private int fruitsRequiredForPowerUp = 5; // 
+    [SerializeField] private Button powerUpButton; // 
+
+
+    private void Start()
+    {
+        // 
+        powerUpButton.gameObject.SetActive(false);
+        powerUpButton.onClick.AddListener(ActivatePowerUp); // 
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -17,6 +27,21 @@ public class FruitCollector : MonoBehaviour
             Destroy(collision.gameObject);
             collectibles++;
             CollectiblesText.text = "Fruits: " + collectibles;
+            if (collectibles >= fruitsRequiredForPowerUp)
+            {
+                // 
+                powerUpButton.gameObject.SetActive(true);
+            }
         }
+    }
+    private void ActivatePowerUp()
+    {
+        // 
+        Debug.Log("Power-Up Activated!");
+
+        // 
+        powerUpButton.gameObject.SetActive(false);
+        collectibles = collectibles- fruitsRequiredForPowerUp;
+        CollectiblesText.text = "Fruits: " + collectibles;
     }
 }
