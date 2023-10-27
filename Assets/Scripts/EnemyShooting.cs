cbe022a8aef18eefbd0c3c11ce201f6a2983b431
AdarshUSC <adarshku@usc.edu>
2023-10-26 22:28:35 -0700

    change in enemy properties in tutorial

--- Assets/Scripts/EnemyShooting.cs
+++ Assets/Scripts/EnemyShooting.cs
@@ -18,7 +18,7 @@ public class EnemyShooting : MonoBehaviour
     {
         float distance = Vector2.Distance(transform.position, player.transform.position);
         Debug.Log("distance is "+ distance);
-        if(distance<8 && !compareColors(GetComponent<SpriteRenderer>().color, player.GetComponent<SpriteRenderer>().color)){
+        if(distance<5 && !compareColors(GetComponent<SpriteRenderer>().color, player.GetComponent<SpriteRenderer>().color)){
             timer+= Time.deltaTime;
             if(timer > 1){
                 timer = 0;  
