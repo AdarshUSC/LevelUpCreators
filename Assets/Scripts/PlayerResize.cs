96a810cd9d54d12824c97f9843147ac10f14ecda
ojasvinaik13 <ojasvi.naik@gmail.com>
2023-11-01 00:08:15 -0700

    Added mushroom counter wrt resizing mechanic

--- Assets/Scripts/PlayerResize.cs
+++ Assets/Scripts/PlayerResize.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerResize : MonoBehaviour
 {
@@ -9,14 +11,16 @@ public class PlayerResize : MonoBehaviour
     private bool ogscale;
     float resizeTimer;
     GameObject player;
+
+    private int mushrooms = 0;
+    [SerializeField] private TMP_Text MushroomsText;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Mushroom")
         {
             Destroy(collision.gameObject);
-            Vector3 newScale = new Vector3(3.0f, 3.0f, 3.0f);
-            playerTransform.localScale = newScale;
-            ogscale = false;
+            mushrooms++;
+            MushroomsText.text = mushrooms.ToString();
         }
     }
         // Start is called before the first frame update
@@ -27,11 +31,23 @@ public class PlayerResize : MonoBehaviour
         originalScale = playerTransform.localScale;
         ogscale = true;
         resizeTimer = Time.deltaTime;
+        mushrooms = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (ogscale && mushrooms>0)
+            {
+                Vector3 newScale = new Vector3(3.0f, 3.0f, 3.0f);
+                playerTransform.localScale = newScale;
+                ogscale = false;
+                mushrooms--;
+                MushroomsText.text = mushrooms.ToString();
+            }
+        }
         if (!ogscale)
         {
             resizeTimer += Time.deltaTime;
