ba9e71bea6eafdf1fb2ce7087d59b7a9265be74b
AdarshUSC <adarshku@usc.edu>
2023-11-02 02:09:55 -0700

    freezing undoing complete

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -12,16 +12,39 @@ public class EnemyPatrol : MonoBehaviour
     Vector2 walkAmount;
     float originalX; // Original float value
 
+    float blueTimer;
+    bool blueOn;
+
 
     void Start () {
         this.originalX = this.transform.position.x;
         wallLeft = transform.position.x - 1f;
         wallRight = transform.position.x + 1f;
+        blueTimer = 0f;
+        blueOn = false;
     }
 
     // Update is called once per frame
     void Update () {
         GameObject icePrefab = gameObject.transform.FindChild("iceCave").gameObject;
+        if(blueOn){
+            blueTimer+= Time.deltaTime;
+            Debug.Log("blue timer is "+ blueTimer);
+            if(blueTimer > 5){
+                blueOn=false;
+                blueTimer = 0.0f;
+                icePrefab.GetComponent<SpriteRenderer>().enabled = false;
+                // foreach(Transform transform in colorPanel.transform) {
+                //     if(transform.CompareTag("ColorButton")) {
+                //         Button colorButton = transform.gameObject.GetComponent<Button>();
+                //         colorButton.interactable=true; // switching off the colors when power up is being used
+                //     }
+                // }
+                // GameObject.FindGameObjectWithTag("mixArea").GetComponent<Button>().interactable=false;
+                // gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+                return;
+            } 
+        }
         SpriteRenderer sr = icePrefab.GetComponent<SpriteRenderer>();
         if(!sr.enabled){
             walkAmount.x = walkingDirection * walkSpeed * Time.deltaTime;
@@ -32,6 +55,21 @@ public class EnemyPatrol : MonoBehaviour
             }
             transform.Translate(walkAmount);
         } 
+    }
+    void OnCollisionEnter2D(Collision2D collision) {
+        
+        Color currColor =  collision.gameObject.GetComponent<SpriteRenderer>()!=null?collision.gameObject.GetComponent<SpriteRenderer>().color:Color.white;
+        if (collision.gameObject.CompareTag("Boomerang") && currColor == Color.blue){
+            Debug.Log("enemy hit");
+            blueOn=true;
+            blueTimer=Time.deltaTime;
+            GameObject icePrefab = gameObject.transform.FindChild("iceCave").gameObject;
+            icePrefab.GetComponent<SpriteRenderer>().enabled = true;
+            // gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+            collision.gameObject.transform.position = collision.gameObject.transform.parent.position;
+            collision.gameObject.gameObject.SetActive(false);
+            // Destroy(gameObject);
+        }
     }
 }
 // 115, 72, 140 // 178, 238, 46 // 55, 199, 163 
