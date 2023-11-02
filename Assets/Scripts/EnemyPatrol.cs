e3c124e27d25f5761dd81a1271831b4f9023724f
AdarshUSC <adarshku@usc.edu>
2023-11-02 01:48:13 -0700

    stopped enemy movement on freezing

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -21,13 +21,17 @@ public class EnemyPatrol : MonoBehaviour
 
     // Update is called once per frame
     void Update () {
-        walkAmount.x = walkingDirection * walkSpeed * Time.deltaTime;
-        if (walkingDirection > 0.0f && transform.position.x >= wallRight) {
-            walkingDirection = -1.0f;
-        } else if (walkingDirection < 0.0f && transform.position.x <= wallLeft) {
-            walkingDirection = 1.0f;
-        }
-        transform.Translate(walkAmount);
+        GameObject icePrefab = gameObject.transform.FindChild("iceCave").gameObject;
+        SpriteRenderer sr = icePrefab.GetComponent<SpriteRenderer>();
+        if(!sr.enabled){
+            walkAmount.x = walkingDirection * walkSpeed * Time.deltaTime;
+            if (walkingDirection > 0.0f && transform.position.x >= wallRight) {
+                walkingDirection = -1.0f;
+            } else if (walkingDirection < 0.0f && transform.position.x <= wallLeft) {
+                walkingDirection = 1.0f;
+            }
+            transform.Translate(walkAmount);
+        } 
     }
 }
 // 115, 72, 140 // 178, 238, 46 // 55, 199, 163 
