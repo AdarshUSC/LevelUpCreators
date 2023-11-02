764d2ac94f3590323c943cf8fa901b6ef622735f
AdarshUSC <adarshku@usc.edu>
2023-11-02 02:27:15 -0700

    green power up power up fully functional

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -4,6 +4,10 @@ public class Tree : MonoBehaviour
 {
     // Public variable to store the tree's fruits
     public GameObject[] fruits;
+    private float greenTimer;
+    private bool greenOn;
+
+    GameObject player;
 
     void Start()
     {
@@ -12,6 +16,45 @@ public class Tree : MonoBehaviour
         {
             fruit.SetActive(false);
         }
+
+        greenOn=false;
+        greenTimer=0.0f;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if(greenOn){
+            greenTimer+= Time.deltaTime;
+            Debug.Log("green timer is "+ greenTimer);
+            if(greenTimer > 5.0f){
+                // foreach(Transform transform in colorPanel.transform) {
+                //     if(transform.CompareTag("ColorButton")) {
+                //         Button colorButton = transform.gameObject.GetComponent<Button>();
+                //         colorButton.interactable=false; // switching off the colors when power up is being used
+                //     }
+                // }
+                greenOn=false;
+                greenTimer = 0.0f;
+                // GameObject.FindGameObjectWithTag("mixArea").GetComponent<Button>().interactable=false;
+                Player.playerMoveSpeed=8.0f;
+                player.GetComponent<SpriteRenderer>().color = Color.white;
+                return;
+            } 
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+
+        Color currColor =  collision.gameObject.GetComponent<SpriteRenderer>()!=null?collision.gameObject.GetComponent<SpriteRenderer>().color:Color.white;
+        if (collision.gameObject.CompareTag("Boomerang") && currColor == Color.green){
+            player.GetComponent<SpriteRenderer>().color = Color.green;
+            greenOn=true;
+            greenTimer=Time.deltaTime;
+            Player.playerMoveSpeed=1.0f;
+            transform.position = transform.parent.position;
+            gameObject.SetActive(false);
+        } 
     }
 
     public void DropFruits()
