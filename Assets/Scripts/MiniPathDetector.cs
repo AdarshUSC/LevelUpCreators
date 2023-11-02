9d464cfbed839759686f7faa4b010a75d9eb28b4
ojasvinaik13 <ojasvi.naik@gmail.com>
2023-11-02 00:26:26 -0700

    Added mini path detection script and trigger

---
+++ Assets/Scripts/MiniPathDetector.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniPathDetector : MonoBehaviour
+{
+    private bool playerInside = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerInside = true;
+            Debug.Log("Player entered the mini path.");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerInside = false;
+            Debug.Log("Player exited the mini path.");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
