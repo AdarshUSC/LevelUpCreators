fcb705d4b6a6d61d6c7b281a6398492f7e012289
Wuhen Duan <63495442+cl-vv-h@users.noreply.github.com>
2023-11-16 16:44:38 -0800

    added mini camera for shadow and Q to get mirror back

--- Assets/Scripts/ReflectionScripts/RelectionPower.cs
+++ Assets/Scripts/ReflectionScripts/RelectionPower.cs
@@ -4,9 +4,12 @@ public class ReflectionPower : MonoBehaviour
 {
     public GameObject mirror;
     public GameObject shadow;
+    public GameObject shadowCamera;
+    public float cameraZoomSize;
 
     private GameObject myMirror;
     private GameObject myShadow;
+    private GameObject myShadowCamera;
     private Vector3 direction;
     private bool isAbletoPut;
     private bool isAbletoGetBack;
@@ -32,22 +35,32 @@ public class ReflectionPower : MonoBehaviour
             {
                 //if (isAbletoGetBack)
                 //{
-                    Debug.Log("Get Mirror Back.");
-                    if (isAbletoGetBack)
-                        gameObject.transform.position = myShadow.transform.position;
-                    Destroy(myMirror);
-                    Destroy(myShadow);
-            //}
-        } else
+                Debug.Log("Get Mirror Back.");
+                if (isAbletoGetBack)
+                    gameObject.transform.position = myShadow.transform.position;
+                Destroy(myMirror);
+                Destroy(myShadow);
+                Destroy(myShadowCamera);
+                //}
+            } else
             {
                 Debug.Log("Mirror Put!");
                 myMirror = Instantiate(mirror, transform.position + direction/5, Quaternion.identity);
                 myShadow = Instantiate(shadow, transform.position + direction/5, Quaternion.identity);
+                myShadowCamera = Instantiate(shadowCamera);
+
                 Player.reflection++;
                 Debug.Log("Adding reflection" + Player.current_mechs.Count);
                 Player.current_mechs.Add("Reflection");
             }
          
+        }
+
+        if (myMirror && Input.GetKeyDown(KeyCode.Q))
+        {
+            Destroy(myMirror);
+            Destroy(myShadow);
+            Destroy(myShadowCamera);
         }
 
         // if any objects block on the way between player and mirror
@@ -65,12 +78,34 @@ public class ReflectionPower : MonoBehaviour
                 myShadow.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
                 isAbletoGetBack = false;
             }
+
+            if (isAbletoGetBack && !myShadowCamera)
+            {
+                myShadowCamera = Instantiate(shadowCamera);
+            } else if (!isAbletoGetBack && myShadowCamera)
+            {
+                Destroy(myShadowCamera);
+            }
         }
+
+        //if (myShadowCamera && !isAbletoGetBack)
+        //{
+        //    Destroy(myShadowCamera);
+        //}
+        //if (!myShadowCamera && isAbletoGetBack)
+        //{
+        //    myShadowCamera = Instantiate(shadowCamera);
+        //}
 
         if (myShadow)
         {
             // need to add when collide with the wall.
             myShadow.transform.position = new Vector2(myMirror.transform.position.x + (myMirror.transform.position.x - gameObject.transform.position.x), gameObject.transform.position.y);
+            if (myShadowCamera)
+            {
+                myShadowCamera.transform.position = new Vector3(myShadow.transform.position.x, myShadow.transform.position.y, -3);
+                myShadowCamera.GetComponent<Camera>().orthographicSize = cameraZoomSize;
+            }
         }
     }
 
